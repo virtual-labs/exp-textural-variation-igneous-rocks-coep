74615e135f7e24d95a593dd05fd0a23da6702648
vleadadmin <admin@vlabs.ac.in>
2023-01-09 11:49:42 +0000

    https://virtual-labs.github.io/virtual-labs/exp-textural-variation-igneous-rocks-coep click on the link to test your code.

---
+++ simulation/src/igneRocksJson.js
@@ -0,0 +1,502 @@
+tempMasterJson = {};
+var masterJsonArr = [];
+var tempQuestionArr = [];
+masterJson = {};
+
+questionAnswer = {
+		"question": "What is the texture called when large-sized crystals are embedded in fine grained matrix?",
+		"correctAns": "Porphyritic texture",
+		"wrongAns1": "Poiklitic texture",
+		"wrongAns2": "Granitic texture",
+		"wrongAns3": "Directive texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate ",
+		"wrongAns2": "Transitional",
+		"wrongAns3": "Secondary"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "Which texture indicates the flow of magma during its formation?",
+		"correctAns": "Directive",
+		"wrongAns1": "Intergrowth",
+		"wrongAns2": "Intergranular",
+		"wrongAns3": "Equigranular"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is it called when two or more minerals crystallize simultaneously in a limited space?",
+		"correctAns": "Intergrowth texture",
+		"wrongAns1": "Directive texture",
+		"wrongAns2": "Interstitial texture",
+		"wrongAns3": "Intergranular texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the absolute reverse of porphyritic texture?",
+		"correctAns": "Poiklitic",
+		"wrongAns1": "Granitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Ophitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the term when augite has inclusions of plagioclase felspar?",
+		"correctAns":"Ophitic",
+		"wrongAns1": "Poiklitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Granitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate",
+		"wrongAns2": "Secondary",
+		"wrongAns3": "Transitional"
+	};
+	tempQuestionArr.push(questionAnswer);
+
+	questionAnswer = {
+		"question": "Pick the option which does not determine the texture of igneous rocks",
+		"correctAns": "Colour",
+		"wrongAns1": "Size",
+		"wrongAns2": "Shape",
+		"wrongAns3": "Arrangement of the constituents within the body of the rock"
+	};
+tempQuestionArr.push(questionAnswer);
+ 
+masterJson = {
+	"mineralId": 1,
+	"dummyName": "Texture1",
+	"textureName": "Phaneritic texture",
+	"src": "images/Phaneritic.jpg",
+	//"src1": "images/Phaneritic1.jpg",
+	//"src2": "images/Phaneritic2.jpg",
+	"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
+
+//Second Rock
+
+tempQuestionArr = [];
+questionAnswer = {
+		"question": "What is the texture called when large-sized crystals are embedded in fine grained matrix?",
+		"correctAns": "Porphyritic texture",
+		"wrongAns1": "Poiklitic texture",
+		"wrongAns2": "Granitic texture",
+		"wrongAns3": "Directive texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate ",
+		"wrongAns2": "Transitional",
+		"wrongAns3": "Secondary"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "Which texture indicates the flow of magma during its formation?",
+		"correctAns": "Directive",
+		"wrongAns1": "Intergrowth",
+		"wrongAns2": "Intergranular",
+		"wrongAns3": "Equigranular"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is it called when two or more minerals crystallize simultaneously in a limited space?",
+		"correctAns": "Intergrowth texture",
+		"wrongAns1": "Directive texture",
+		"wrongAns2": "Interstitial texture",
+		"wrongAns3": "Intergranular texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the absolute reverse of porphyritic texture?",
+		"correctAns": "Poiklitic",
+		"wrongAns1": "Granitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Ophitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the term when augite has inclusions of plagioclase felspar?",
+		"correctAns":"Ophitic",
+		"wrongAns1": "Poiklitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Granitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate",
+		"wrongAns2": "Secondary",
+		"wrongAns3": "Transitional"
+	};
+	tempQuestionArr.push(questionAnswer);
+
+	questionAnswer = {
+		"question": "Pick the option which does not determine the texture of igneous rocks",
+		"correctAns": "Colour",
+		"wrongAns1": "Size",
+		"wrongAns2": "Shape",
+		"wrongAns3": "Arrangement of the constituents within the body of the rock"
+	};
+tempQuestionArr.push(questionAnswer);
+ 
+masterJson = {
+	"mineralId": 2,
+	"dummyName": "Texture2",
+	"textureName": "Aphanitic texture",
+	"src": "images/Rhyolite 1.jpg",
+	"src1": "images/Rhyolite 2.jpg",
+	"src2": "images/Rhyolite 3.jpg",
+	"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
+
+//Thired Rock
+
+
+tempQuestionArr = [];
+questionAnswer = {
+		"question": "What is the texture called when large-sized crystals are embedded in fine grained matrix?",
+		"correctAns": "Porphyritic texture",
+		"wrongAns1": "Poiklitic texture",
+		"wrongAns2": "Granitic texture",
+		"wrongAns3": "Directive texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate ",
+		"wrongAns2": "Transitional",
+		"wrongAns3": "Secondary"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "Which texture indicates the flow of magma during its formation?",
+		"correctAns": "Directive",
+		"wrongAns1": "Intergrowth",
+		"wrongAns2": "Intergranular",
+		"wrongAns3": "Equigranular"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is it called when two or more minerals crystallize simultaneously in a limited space?",
+		"correctAns": "Intergrowth texture",
+		"wrongAns1": "Directive texture",
+		"wrongAns2": "Interstitial texture",
+		"wrongAns3": "Intergranular texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the absolute reverse of porphyritic texture?",
+		"correctAns": "Poiklitic",
+		"wrongAns1": "Granitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Ophitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the term when augite has inclusions of plagioclase felspar?",
+		"correctAns":"Ophitic",
+		"wrongAns1": "Poiklitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Granitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate",
+		"wrongAns2": "Secondary",
+		"wrongAns3": "Transitional"
+	};
+	tempQuestionArr.push(questionAnswer);
+
+	questionAnswer = {
+		"question": "Pick the option which does not determine the texture of igneous rocks",
+		"correctAns": "Colour",
+		"wrongAns1": "Size",
+		"wrongAns2": "Shape",
+		"wrongAns3": "Arrangement of the constituents within the body of the rock"
+	};
+tempQuestionArr.push(questionAnswer);
+ 
+masterJson = {
+	"mineralId": 3,
+	"dummyName": "Texture3",
+	"textureName": "Porphyritic texture", 
+	"src": "images/Porphyritic granite 1.jpg",
+	"src1": "images/Porphyritic granite 2.jpg",
+	"src2": "images/Porphyritic granite 3.jpg",
+	"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
+
+//Fourth Rock
+
+tempQuestionArr = [];
+
+questionAnswer = {
+		"question": "What is the texture called when large-sized crystals are embedded in fine grained matrix?",
+		"correctAns": "Porphyritic texture",
+		"wrongAns1": "Poiklitic texture",
+		"wrongAns2": "Granitic texture",
+		"wrongAns3": "Directive texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate ",
+		"wrongAns2": "Transitional",
+		"wrongAns3": "Secondary"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "Which texture indicates the flow of magma during its formation?",
+		"correctAns": "Directive",
+		"wrongAns1": "Intergrowth",
+		"wrongAns2": "Intergranular",
+		"wrongAns3": "Equigranular"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is it called when two or more minerals crystallize simultaneously in a limited space?",
+		"correctAns": "Intergrowth texture",
+		"wrongAns1": "Directive texture",
+		"wrongAns2": "Interstitial texture",
+		"wrongAns3": "Intergranular texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the absolute reverse of porphyritic texture?",
+		"correctAns": "Poiklitic",
+		"wrongAns1": "Granitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Ophitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the term when augite has inclusions of plagioclase felspar?",
+		"correctAns":"Ophitic",
+		"wrongAns1": "Poiklitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Granitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate",
+		"wrongAns2": "Secondary",
+		"wrongAns3": "Transitional"
+	};
+	tempQuestionArr.push(questionAnswer);
+
+	questionAnswer = {
+		"question": "Pick the option which does not determine the texture of igneous rocks",
+		"correctAns": "Colour",
+		"wrongAns1": "Size",
+		"wrongAns2": "Shape",
+		"wrongAns3": "Arrangement of the constituents within the body of the rock"
+	};
+
+	masterJson = {
+		"mineralId": 4,
+		"dummyName": "Texture4",
+		"textureName": "Glassy texture",
+		"src": "images/Glassy1.jpg",
+		"src1": "images/Glassy2.jpg",
+		"src2": "images/Glassy3.jpg",
+		"question": tempQuestionArr
+	};
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
+
+//Fifth Rock
+tempQuestionArr = [];
+questionAnswer = {
+		"question": "What is the texture called when large-sized crystals are embedded in fine grained matrix?",
+		"correctAns": "Porphyritic texture",
+		"wrongAns1": "Poiklitic texture",
+		"wrongAns2": "Granitic texture",
+		"wrongAns3": "Directive texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate ",
+		"wrongAns2": "Transitional",
+		"wrongAns3": "Secondary"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "Which texture indicates the flow of magma during its formation?",
+		"correctAns": "Directive",
+		"wrongAns1": "Intergrowth",
+		"wrongAns2": "Intergranular",
+		"wrongAns3": "Equigranular"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is it called when two or more minerals crystallize simultaneously in a limited space?",
+		"correctAns": "Intergrowth texture",
+		"wrongAns1": "Directive texture",
+		"wrongAns2": "Interstitial texture",
+		"wrongAns3": "Intergranular texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the absolute reverse of porphyritic texture?",
+		"correctAns": "Poiklitic",
+		"wrongAns1": "Granitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Ophitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the term when augite has inclusions of plagioclase felspar?",
+		"correctAns":"Ophitic",
+		"wrongAns1": "Poiklitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Granitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate",
+		"wrongAns2": "Secondary",
+		"wrongAns3": "Transitional"
+	};
+	tempQuestionArr.push(questionAnswer);
+
+	questionAnswer = {
+		"question": "Pick the option which does not determine the texture of igneous rocks",
+		"correctAns": "Colour",
+		"wrongAns1": "Size",
+		"wrongAns2": "Shape",
+		"wrongAns3": "Arrangement of the constituents within the body of the rock"
+	};
+tempQuestionArr.push(questionAnswer);
+ 
+masterJson = {
+	"mineralId": 5,
+	"dummyName": "Texture5",
+	"textureName": "Pyroclastic texture",
+	"src": "images/Pyroclastic.jpg",
+//	"src1": "images/Pyroclastic1.jpg",
+//	"src2": "images/Pyroclastic2.jpg",
+	"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
+
+//Sixth Rock
+
+tempQuestionArr = [];
+questionAnswer = {
+		"question": "What is the texture called when large-sized crystals are embedded in fine grained matrix?",
+		"correctAns": "Porphyritic texture",
+		"wrongAns1": "Poiklitic texture",
+		"wrongAns2": "Granitic texture",
+		"wrongAns3": "Directive texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate ",
+		"wrongAns2": "Transitional",
+		"wrongAns3": "Secondary"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "Which texture indicates the flow of magma during its formation?",
+		"correctAns": "Directive",
+		"wrongAns1": "Intergrowth",
+		"wrongAns2": "Intergranular",
+		"wrongAns3": "Equigranular"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is it called when two or more minerals crystallize simultaneously in a limited space?",
+		"correctAns": "Intergrowth texture",
+		"wrongAns1": "Directive texture",
+		"wrongAns2": "Interstitial texture",
+		"wrongAns3": "Intergranular texture"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the absolute reverse of porphyritic texture?",
+		"correctAns": "Poiklitic",
+		"wrongAns1": "Granitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Ophitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "What is the term when augite has inclusions of plagioclase felspar?",
+		"correctAns":"Ophitic",
+		"wrongAns1": "Poiklitic",
+		"wrongAns2": "Felsitic",
+		"wrongAns3": "Granitic"
+	};
+	tempQuestionArr.push(questionAnswer);
+	questionAnswer = {
+		"question": "The texture which the characteristics in between granitic and felsitic are",
+		"correctAns": "Orthophyric",
+		"wrongAns1": "Intermediate",
+		"wrongAns2": "Secondary",
+		"wrongAns3": "Transitional"
+	};
+	tempQuestionArr.push(questionAnswer);
+
+	questionAnswer = {
+		"question": "Pick the option which does not determine the texture of igneous rocks",
+		"correctAns": "Colour",
+		"wrongAns1": "Size",
+		"wrongAns2": "Shape",
+		"wrongAns3": "Arrangement of the constituents within the body of the rock"
+	};
+tempQuestionArr.push(questionAnswer);
+ 
+masterJson = {
+	"mineralId": 6,
+	"dummyName": "Texture6",
+	"textureName": "Pegmatitic texture",
+	"src": "images/Pegmatite 1.jpg",
+	"src1": "images/Pegmatite 2.jpg",
+	"src2": "images/Pegmatite 3.jpg",
+
+	"question": tempQuestionArr
+};
+
+masterJsonArr.push(masterJson);
+tempMasterJson.demo = masterJsonArr;
+
+
+
+
+
+
+
+
+
---
+++ theory.md
@@ -0,0 +1,41 @@
+### Important textural types in IGNEOUS ROCKS
+Texture is defined on the basis of glass vs. mineral grains proportions, their sizes, shapes and mutual arrangements. These textures and structures provide an idea about the geological processes during its crystallisation. They also provide us valuable information about physical chemistry and cooling history of the igneous rocks.
+The articulation of mineral grains is described in terms of planar, smoothly curved, sinuous, sutured, interlocked, or irregular surfaces of mutual boundary. The distribution and orientation of mineral grains and of mineral grains and glass are other elements of fabric that can be useful in estimating the conditions and sequence of mineral formation in igneous rocks
+### Crystallinity is largely controlled and governed by following factors:
+- Rate of cooling- Faster the cooling finer will be the crystals. Volcanic glass is formed by rapid cooling of the lava. Slower rate of cooling favours crystal growth of the bigger crystals.
+- Depth of cooling and volume of magma- Higher the depth, slower will be the cooling rate as heat deccipiation is slow. Similarly, volume of the magma have significant bearing on crystal growth. Larger the volume slower will be the rate of cooling, hence larger crystals will be formed.
+- Composition and viscosity of magma- Highly viscous lavas such as rhyolite or siliceous magma favours formation of non-crystalline and/or glassy rocks. Contrarily, less viscous basaltic or mafic lavas/magmas give rise to crystalline rocks e.g. phyric and aphyric basalts. Magma with high viscosity have high volatile content as compared to less viscous magmas
+##### The following are only a few of the most important examples:
+Directive textures are produced by the preferred orientation of platy, tabular, or elongate mineral grains to yield grossly planar or linear arrangements; they are generally a result of magmatic flowage.
+##### Graphic texture
+refers to the regular intergrowth of two minerals, one of them generally serving as a host and the other appearing on surfaces of the host as striplike or cuneiform units with grossly consistent orientation; the graphic intergrowth of quartz in alkali feldspar is a good example.
+##### Ophitic texture 
+is the association of lath-shaped euhedral crystals of plagioclase, grouped radially or in an irregular mesh, with surrounding or interstitial large anhedral crystals of pyroxene; it is characteristic of the common rock type known as diabase.
+##### Poikilitic texture
+describes the occurrence of one mineral that is irregularly scattered as diversely oriented crystals within much larger host crystals of another mineral.
+##### Reaction texture
+occur at the corroded margins of crystals, from the corrosive rimming of crystals of one mineral by finer-grained aggregates of another, or as a result of other features that indicate partial removal of crystalline material by reaction with magma or other fluid.
+##### Pyroclastic texture
+results from the explosive fragmentation of volcanic material, including magma (commonly the light, frothy pumice variety and glass fragments called shards), country rock, and phenocrysts. Fragments less than 2 millimetres in size are called ash, and the rock formed of these is called tuff; fragments between 2 and 64 millimetres are lapilli and the rock is lapillistone; fragments greater than 64 millimetres are called bombs if rounded or blocks if angular, and the corresponding rock is termed agglomerate or pyroclastic breccia, respectively. Commonly, many of these pyroclastic rocks have been formed by dense hot clouds that hug the ground and behave much like a lava flow and hence are given the name pyroclastic flow.
+Replacement textures occur where a mineral or mineral aggregate has the external crystal form of a preexisting different mineral (pseudomorphism) or where the juxtaposition of two minerals indicates that one was formed at the expense of the other.
+### Aphanitic Texture
+![aphanitic*************************************************](images/aphanitic.jpg)
+<br>Aphanitic rocks have mineral grains that are mostly too small to be seen with the naked eye or a hand lens, like this Rhyolite. Basalt is another igneous rock with the aphanitic texture. 
+### Equigranular Texture
+![Equigranular*************************************************](images/Equigranular.jpg)
+<br>Rocks with EQUIGRANULAR have mineral grains that are generally the same size. This example is a GRANITE.
+### Glassy Texture
+![glassy*************************************************](images/glassy.jpg)
+<br>Glassy (or hyaline or vitreous) rocks have no or almost no grains at all, as in this quickly chilled pahoehoe basalt or in OBSIDIAN..
+### Phaneritic Texture
+![phaneritic*************************************************](images/phaneritic.jpg)
+<br>Phaneritic rocks have mineral grains that are large enough to be seen with the naked eye or a hand lens, like this GRANITE
+### Poikilitic Texture
+![Poikilitic*************************************************](images/Poikilitic.jpg)
+<br>Poikilitic  texture is one in which large crystals, like this feldspar grain, contain small grains of other minerals scattered inside them And it is commonly found in rock  SYENITE.
+### Pyroclastic Texture
+![Pyroclastic*************************************************](images/pyroclastic.jpg)
+<br>Rocks with pyroclastic texture are made of pieces of volcanic material that are created in an explosive eruption, like this welded tuff. Pyroclastic texture results from the explosive fragmentation of volcanic material, including magma (commonly the light, frothy pumice variety and glass fragments called shards), country rock, and phenocrysts.eg-PUMICE
+### Vesicular Texture
+![vesicular*************************************************](images/vesicular.jpg)
+<br>Rocks with vesicular texture are full of bubbles. It always indicates a volcanic rock, like this SCORIA.
